271eb3680fff0f6668ec587fb58a9ccc9c0c29f8
KHW2019 <gabrielwang376@gmail.com>
2024-09-13 18:09:55 +0100

    remove_outdated_comment

--- src/Feature/ReportGeneration/ReportGeneration.cpp
+++ src/Feature/ReportGeneration/ReportGeneration.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 void generateAccountReport(const Bank& bank) {
-    bank.displayAccounts();  // Assuming this function displays all accounts
-    // Additional reporting logic could be added here
+    bank.displayAccounts();  
+    
 }
